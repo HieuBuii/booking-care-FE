aad14ce03609f061a1f92e6c473e30014c4d7ad5
Hieu Bui <hieusevenxb@gmail.com>
2022-10-20 10:49:34 +0700

    CRUD update

--- src/services/userService.js
+++ src/services/userService.js
@@ -3,4 +3,26 @@ const handleLoginApi = (userEmail, userPassword) => {
   return axios.post("/api/login", { email: userEmail, password: userPassword });
 };
 
-export { handleLoginApi };
+const getAllUsers = (userId) => {
+  return axios.get(`/api/get-all-users?id=${userId}`);
+};
+
+const addUserService = (data) => {
+  return axios.post("/api/create-user", data);
+};
+
+const deleteUserService = (userId) => {
+  return axios.delete("/api/delete-user", { data: { id: userId } });
+};
+
+const editUserService = (inputData) => {
+  return axios.put("/api/edit-user", inputData);
+};
+
+export {
+  handleLoginApi,
+  getAllUsers,
+  addUserService,
+  deleteUserService,
+  editUserService,
+};
